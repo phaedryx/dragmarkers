4069462a8d5f2f2c413a8f31d43605a30a1313ba
Tad Thorley <phaedryx@gmail.com>
2011-11-14 18:57:54 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dragmarkers::Application.config.secret_token = '118964fd4e3fa3cea366a95be5c8921ebb3b7aa27c2dee7ec02adfb473805178d1b4ed1e578bf2dc2be2c9fdc8c7e090da4e48973e3facbb8daf7a48c4153e25'
