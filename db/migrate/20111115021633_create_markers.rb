691e4d796e2d438d2da20f32ce5237cdd8326ab3
Tad Thorley <phaedryx@gmail.com>
2011-11-14 19:21:39 -0700

    create marker schema

--- db/migrate/20111115021633_create_markers.rb
+++ db/migrate/20111115021633_create_markers.rb
@@ -1,7 +1,9 @@
 class CreateMarkers < ActiveRecord::Migration
   def change
     create_table :markers do |t|
-
+      t.decimal :latitude
+      t.decimal :longitude
+      t.string  :icon
       t.timestamps
     end
   end
